59b7045623eb7974ad6d391aab9db208c8b8fc20
drscook <scook@tarleton.edu>
2022-11-01 15:45:40 +0000

    changes

--- model.py
+++ model.py
@@ -1,14 +1,22 @@
 from acs5 import *
 import torch, matplotlib.pyplot as plt, pandas_bokeh
 device = "cuda" if torch.cuda.is_available() else "cpu"
+warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
 
-def features(year, election='general', office='President', level='tract', overwrite=False):
+def tensorify(T):
+    if not torch.is_tensor(T):
+        T = torch.FloatTensor(np.array(T)).to(device)
+    return T
+
+def extract_features(elec, overwrite=False):
+# def extract_features(year, election='general', office='President', level='tract', overwrite=False):
+    year, election, office = elec.split('_')
     m_per_mi = 1609.34
     year = int(year)
     get_acs5(year=year, overwrite=overwrite)
     pq = DATA_PATH / f'features/{year}_{election}_{office}.parquet'
     if pq.is_file() and not overwrite:
-        df = pd.read_parquet(pq)
+        df = gpd.read_parquet(pq)
     else:
         qry = f"""
         select
@@ -33,6 +41,7 @@ def features(year, election='general', office='President', level='tract', overwr
             A.dist_to_border / {m_per_mi} as dist_to_border,
             A.aland / {m_per_mi**2} as aland,
             A.polsby_popper as polsby_popper,
+            A.geometry,
         from
             shapes.vtd2020 as A
         """
@@ -44,7 +53,8 @@ def features(year, election='general', office='President', level='tract', overwr
             acs5.{year}
         """
         acs = query_to_df(qry).set_index('vtd2020')
-        vtd = prep(elections.join(shapes, how='inner').join(acs, how='inner'))
+        vtd = prep(gpd.GeoDataFrame(elections.join(shapes, how='inner').join(acs, how='inner')))
+        vtd.geometry = vtd.geometry.simplify(0.004)
         df = vtd
 
         for race in ['all', 'white', 'hisp', 'other']:
@@ -67,3 +77,13 @@ def features(year, election='general', office='President', level='tract', overwr
         df[col].fillna(df[col].median(), inplace=True)
         df_to_parquet(df, pq)
     return df
+
+def extract_dataset(elections, feat, targ, weig):
+    D = [extract_features(elec) for elec in listify(elections)]
+    df = gpd.GeoDataFrame(pd.concat(D).sample(frac=1, random_state=42)).to_crs(D[0].crs)
+    W = df[weig].astype(float)
+    X = df[feat].astype(float)
+    Y = df[targ].astype(float)
+    W = W.join(W, lsuffix='_d', rsuffix='_r')
+    X = (X - X.min()) / (X.max() - X.min())
+    return df, W, X, Y
