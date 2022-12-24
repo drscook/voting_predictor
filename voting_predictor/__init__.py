3f0cc2244069dcd40fc3be15606f2937694885b0
drscook <scook@tarleton.edu>
2022-12-24 12:08:12 -0600

    patch

--- voting_predictor/__init__.py
+++ voting_predictor/__init__.py
@@ -1,3 +1,7 @@
-from helpers.helpers.common_imports import *
-from helpers.helpers import utilities as ut
+try:  # tries local version
+    from helpers.helpers.common_imports import *
+    from helpers.helpers import utilities as ut
+except ModuleNotFoundError:  # fail over to github version
+    from helpers.common_imports import *
+    from helpers import utilities as ut
 from .main import *
