9a561959eacd26bd31d94ff00dfc4afed90563e6
Scott Cook <scook@tarleton.edu>
2022-12-24 01:35:48 -0600

    Update main.py

--- voting_predictor/main.py
+++ voting_predictor/main.py
@@ -8,6 +8,6 @@ class Redistricter():
     state: str = 'TX'
     
     def __post_init__(self):
-        self.census = census.Census(self.api_key)
+        self.census = census.Census(self.census_api_key)
         self.bq = ut.BigQuery(project_id=self.bq_project_id)
         self.state = us.STATES.lookup(self.state)
