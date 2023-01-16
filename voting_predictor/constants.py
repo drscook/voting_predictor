05ba3159178d0d9772e7646f3deac93b58097462
Scott Cook <scook@tarleton.edu>
2023-01-15 22:31:52 -0600

    Update constants.py

--- voting_predictor/constants.py
+++ voting_predictor/constants.py
@@ -198,3 +198,6 @@ features = {
     'hisp_vap_spanishathome': ['b16004_026e'],
     'hisp_vap_spanishathomeenglishwell': ['b16004_027e', 'b16004_028e'],
 }
+
+F = {f[f.find('_'):] for f in features.keys()}
+features_universal = {f[1:] for f in F if 'all'+f in features.keys() and 'white'+features.keys() in F and 'hisp'+f in features.keys()}
