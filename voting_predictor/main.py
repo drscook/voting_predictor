e2b87c41eb46bd4fbcce1163a10c446aaee6e99e
Scott Cook <scook@tarleton.edu>
2023-01-15 23:36:56 -0600

    Update main.py

--- voting_predictor/main.py
+++ voting_predictor/main.py
@@ -268,6 +268,14 @@ from (
     {ut.subquery(qry)}
 )"""
             
+            qry = f"""
+select
+    *,
+    {ut.make_select([f'{x} / max({x}) over () as {x}_rel' for x in ['dist_to_border', *[x+'_dens' for x in subpops.keys()]])},
+from (
+    {ut.subquery(qry)}
+)"""
+            
 #              qry = f"""
 # select
 #     A.*,
