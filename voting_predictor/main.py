cbdcf5a6bb92aeb290c708c911fa97c93c793f96
Scott Cook <scook@tarleton.edu>
2023-01-25 18:36:06 -0600

    Update main.py

--- voting_predictor/main.py
+++ voting_predictor/main.py
@@ -574,8 +574,6 @@ using ({geoid})"""
                 download(zip_file, url, unzip=False)
                 d = decade % 100
                 repl = {'vtdkey':f'vtd{year}', f'geoid{d}':geoid, f'aland{d}': 'aland', f'awater{d}': 'awater', 'geometry':'geometry',}
-#                 df = ut.prep(gpd.read_file(zip_file)).rename(columns=repl).to_crs(CRS['bigquery'])
-#                 df.geometry = df.geometry.buffer(0).apply(orient, args=(1,))
                 df = ut.prep(gpd.read_file(zip_file)).rename(columns=repl)
                 df.geometry = df.geometry.to_crs(CRS['bigquery']).buffer(0).apply(orient, args=(1,))
                 self.bq.df_to_tbl(df[df.columns.intersection(repl.values())], tbl)
