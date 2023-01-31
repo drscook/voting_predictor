a33b7f83ee73747efcacfbfadb870486d84d7650
Scott Cook <scook@tarleton.edu>
2023-01-30 19:58:38 -0600

    Update main.py

--- voting_predictor/main.py
+++ voting_predictor/main.py
@@ -198,7 +198,6 @@ from (
                 cols = [self.geoid, 'office', 'year', 'election', 'name', 'party', 'incumbent', 'votes']                    
                 for file in path.iterdir():
                     a = ut.prep(file.stem.split('_'))
-                    print(file.stem, a)
                     if ('general' in a) & ('returns' in a):
                         df = ut.prep(pd.read_csv(file)).rename(columns={'vtdkeyvalue':self.geoid})
                         mask = (df['votes'] > 0) & (df['party'].isin(('R', 'D', 'L', 'G')))
