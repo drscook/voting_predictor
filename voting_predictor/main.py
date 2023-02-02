60320b7607f8ff9b83b41c9079117d0f6482a1c4
Scott Cook <scook@tarleton.edu>
2023-02-02 16:16:34 -0600

    Update main.py

--- voting_predictor/main.py
+++ voting_predictor/main.py
@@ -416,6 +416,9 @@ join (
                 txt = zip_file.with_name(f'{zip_file.stem}_{self.state.abbr}.txt'.lower())
                 repl = {'blk_2010': 'block_2010', 'blk_2020': 'block_2020', 'arealand_int': 'aland', 'areawater_int':'awater'}
                 df = ut.prep(pd.read_csv(txt, sep='|')).rename(columns=repl)
+                print(df.dtypes)
+                assert 1==2
+                mask = df['state_2010']
                 for year in [2010, 2020]:
                     geoid = self.get_geoid(df, level='block', year=year)
                 self.df_to_tbl(df, tbl, cols=['block2010', 'block2020', 'aland', 'awater'])
