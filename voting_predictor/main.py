742ba165a83306ea5bb971d0e0eac03be7461d8c
Scott Cook <scook@tarleton.edu>
2023-02-03 15:24:40 -0600

    Update main.py

--- voting_predictor/main.py
+++ voting_predictor/main.py
@@ -250,7 +250,7 @@ left join (
             sel_grp = {x:f'sum(case when S.{g(x)} > 0 then A.{x} * I.{g(x)} / S.{g(x)} else A.{x} / S.ct end) as {x}' for x in feat_acs if not "all" in x}
 #             sel_grp = {x:f'sum(A.{x} * I.{g(x)} / greatest(1, S.{g(x)})) as {x}' for x in feat_acs if not "all" in x}
             sel_all = {x:f'{x.replace("all", "hisp")} + {x.replace("all", "other")} + {x.replace("all", "white")} as {x}' for x in feat_acs if "all" in x}
-            sel_den = {x.replace("pop", "den"):f'{x} / areatot * 1000000 as {x.replace("pop", "den")}' for x in subpops.keys()}
+            sel_den = {x.replace("pop", "den"):f'A.{x} / areatot * 1000000 as {x.replace("pop", "den")}' for x in subpops.keys()}
 #             sel_geo = {x:f'min(T.{x}) as {x}' for x in ['dist_to_border', 'arealand', 'areawater', 'areatot', 'areacomputed', 'perimcomputed', 'polsby_popper']}
             feat_geo = ['dist_to_border', 'arealand', 'areawater', 'areatot', 'areacomputed', 'perimcomputed', 'polsby_popper']
             qry = f"""
